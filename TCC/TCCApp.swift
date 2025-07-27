04f0b1fe1c280ac64705e133d8268bb3e3b7f72e
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-07-27 14:34:32 -0300

    Initial Commit

---
+++ TCC/TCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCCApp.swift
+//  TCC
+//
+//  Created by Felipe Ribeiro on 27/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
