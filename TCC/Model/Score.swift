8ace9075178a23b3e0c52e354cfd28c3ad608948
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-08-10 14:43:43 -0300

    Add `Score` type

---
+++ TCC/Model/Score.swift
@@ -0,0 +1,29 @@
+//
+//  Score.swift
+//  TCC
+//
+//  Created by Felipe Ribeiro on 09/08/25.
+//
+
+import Foundation
+
+enum ScoreValues: Int {
+    case zero = 0
+    case forty = 40
+    case eighty = 80
+    case oneTwenty = 120
+    case oneSixty = 160
+    case twoHundred = 200
+}
+
+struct Score {
+    var c1: ScoreValues
+    var c2: ScoreValues
+    var c3: ScoreValues
+    var c4: ScoreValues
+    var c5: ScoreValues
+    
+    var total: Int {
+        [c1, c2, c3, c4, c5].map(\.rawValue).reduce(0, +)
+    }
+}
