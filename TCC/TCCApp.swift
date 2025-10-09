4e8a336605073b94cfc4455a241737aba77a2b65
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-10-09 11:06:33 -0300

    fix: correctly load containers SwiftData containers

--- TCC/TCCApp.swift
+++ TCC/TCCApp.swift
@@ -5,13 +5,29 @@
 //  Created by Felipe Ribeiro on 27/07/25.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct TCCApp: App {
+    let container: ModelContainer
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .modelContainer(container)
+    }
+    
+    init() {
+        do {
+            #if DEBUG
+            container = SampleData.shared.modelContainer
+            #else
+            container = try ModelContainer(for: Topic.self, Essay.self, EssayIteration.self)
+            #endif
+        } catch {
+            fatalError("Error creating ModelContainer: \(error.localizedDescription).")
         }
     }
 }
