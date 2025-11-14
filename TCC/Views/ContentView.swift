4c4ae1f6b194ac07bd079b09a67950cdad9987c8
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2025-11-14 20:32:37 -0300

    feat: handle intent execution in `ContentView`

--- TCC/Views/ContentView.swift
+++ TCC/Views/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Felipe Ribeiro on 27/07/25.
 //
 
+import AppIntents
 import SwiftUI
 import SwiftData
 
@@ -12,21 +13,25 @@ struct ContentView: View {
     @AppStorage("needsOnboarding") var needsOnboarding = true
     
     @State private var path = NavigationPath()
+    @State private var selectedTab: NavigationOption = .essays
     
     var body: some View {
-        TabView {
-            Tab("Redações", systemImage: "book.pages.fill") {
+        TabView(selection: $selectedTab) {
+            Tab("Redações", systemImage: "book.pages", value: .essays) {
                 NavigationStack(path: $path) {
                     EssayListView(path: $path)
                 }
             }
             
-            Tab("Guia", systemImage: "text.book.closed") {
+            Tab("Guia", systemImage: "text.book.closed", value: .guide) {
                 GuideView()
             }
         }
         .fullScreenCover(isPresented: $needsOnboarding) {
             OnboardingView()
+        }
+        .onAppIntentExecution(NavigateIntent.self) { intent in
+            selectedTab = intent.navigationOption
         }
     }
 }
